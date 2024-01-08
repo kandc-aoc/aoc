652726c4136bf542dff295a01c6860ee50f7b4f3
kandc-aoc <kandc@waterdancer.co.uk>
2024-01-08 16:50:41 +0100

    Replaced deleted section of code.

--- day1/puzzle2/puzzle2.py
+++ day1/puzzle2/puzzle2.py
@@ -8,6 +8,8 @@
 import regex as re
 import csv
 
+#line_count=0
+
 grand_total=0
 
 dict  = {'one': '1', 'two': '2', 'three': '3', 'four' : '4', 'five' : '5',
@@ -28,6 +30,17 @@ def combine_to_int(firstnum, lastnum):
     f = dict[firstnum]
     l = dict[lastnum]
     return int (f + l)
+
+
+with open('puzzle_input.txt') as test:
+    for line in test:
+        firstnum,lastnum=find_words(line)
+        result=combine_to_int(firstnum,lastnum)
+        #print("first "+firstnum+" last "+lastnum)
+        grand_total=grand_total+result
+    print (grand_total)
+
+
 
 
 
