8ce8598b3db04b5cd75a34768c27e3dcd0d58c11
kandc-aoc <kandc@waterdancer.co.uk>
2024-01-08 15:08:45 +0100

    Added data structure and function to convert two numeric strings to a single combined int.

--- day1/puzzle2/puzzle2.py
+++ day1/puzzle2/puzzle2.py
@@ -4,3 +4,14 @@
 # the first match can be retrieved by referencing r[0]
 # and the last can be retrieved by referencing r[-1]
 #
+
+
+dict = {'one': '1', 'two': '2', 'three': '3', 'four' : '4', 'five' : '5',
+           'six' : '6', 'seven' : '7', 'eight' : '8', 'nine' : '9', '1' : '1', '2' : '2',
+           '3' : '3', '4' : '4', '5' : '5', '6' : '6', '7' : '7', '8' : '8', '9' : '9'}
+
+def combine_to_int(first, last):
+    f = dict[first]
+    l = dict[last]
+    return int (f + l)
+
