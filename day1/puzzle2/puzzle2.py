3e6598d1446834bf5f40518634ffdc19d3334915
kandc-aoc <kandc@waterdancer.co.uk>
2024-01-08 14:53:11 +0100

    Added comments on the regex for finding numbers.

---
+++ day1/puzzle2/puzzle2.py
@@ -0,0 +1,6 @@
+# The regex
+# r = re.findall('one|two|three|four|five|six|seven|eight|nine|\d', x)
+# returns a list of all matches
+# the first match can be retrieved by referencing r[0]
+# and the last can be retrieved by referencing r[-1]
+#
