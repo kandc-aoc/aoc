4659b7e8f78e0436ca650cb4d3526a41e01fd7c9
kandc-aoc <kandc@waterdancer.co.uk>
2024-01-09 10:36:50 +0100

    Added a function that checks the contents of a line for the 'number color' pattern and returns false if all numbers are less than the total provided.

--- day2/puzzle1/puzzle1.py
+++ day2/puzzle1/puzzle1.py
@@ -1,9 +1,24 @@
-import re
-
-import regex
+import regex as re
 
 with open('example_input.txt') as test:
     for line in test:
         breakup = [j for i in line.split(':') for j in i.split(';')]
         print(line,breakup)
+
+
+s = 'Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green'
+
+#regex checking function
+def is_possible(line, color, total):
+    p = '(\d+)\s+'+color
+    cp = re.compile(p)
+    m = cp.findall(line)
+    for match in m:
+        if int(match) > total:
+            return False
+    return True
+
+
+print(is_possible(s, 'blue', 14))
+
 
