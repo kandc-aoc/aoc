ac70444ba2683b0bd42a464abc2706b62857cd0a
waterdancerk <waterdancerk@gmail.com>
2024-01-10 20:40:54 +0100

    Update puzzle2.py
    edited function

--- day2/puzzle2/puzzle2.py
+++ day2/puzzle2/puzzle2.py
@@ -7,23 +7,22 @@ s = 'Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green'
 
 #regex checking function
 #EDIT to output the max value for each colour cubed
-def is_possible(line, color, total):
-    p = '(\d+)\s+'+color
+def max_color(line,color):
+    p = '(\d+)\s'+color
     cp = re.compile(p)
     m = cp.findall(line)
+    colormax=0
     for match in m:
-        if int(match) > total:
-            return False
-    return True
+        if int(match) > colormax:
+                colormax=int(match)
+    return colormax
 
 
-print(is_possible(s, 'blue', 14))
+#print(max_color(s,'blue'))
 
 #edit to add grand total without skipping games
 with open('puzzle_input2.txt') as test:
     for line in test:
-        game=game+1
-        if (is_possible(line,'blue',14) & is_possible(line,'red',12) & is_possible(line,'green',13)):
-            grand_total=grand_total+game
+
     print (grand_total)
 
