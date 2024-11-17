854d6204bb2fa875071ad03c00738e031c5d3151
waterdancerk <waterdancerk@gmail.com>
2024-11-17 21:11:26 +0000

    test push

--- day5/puzzle1/puzzle1.py
+++ day5/puzzle1/puzzle1.py
@@ -6,7 +6,7 @@ headings = ['seed-to-soil map:', 'soil-to-fertilizer map:',
             'fertilizer-to-water map:', 'water-to-light map:',
             'light-to-temperature map:', 'temperature-to-humidity map:',
             'humidity-to-location map:']
-
+#test comment
 def get_next_input(input, des_base, src_base, range):
     if input >= src_base and input <= src_base+range:
         return des_base + input-src_base
